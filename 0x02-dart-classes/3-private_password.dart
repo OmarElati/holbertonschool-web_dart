3168fd0441a93ccaf4e74386fcbed0570c66f67f
OmarElati <5796@holbertonstudents.com>
2024-09-01 11:34:36 +0100

    Private Password

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,25 +1,14 @@
 class Password {
-  String _password;
+  String? _password;
 
-  Password({required String password}) : _password = password;
+  Password({required String? password}) : _password = password;
 
   bool isValid() {
-    if (_password.length < 8 || _password.length > 16) {
-      return false;
-    }
-
-    if (!_password.contains(RegExp(r'[A-Z]'))) {
-      return false;
-    }
-
-    if (!_password.contains(RegExp(r'[a-z]'))) {
-      return false;
-    }
-
-    if (!_password.contains(RegExp(r'[0-9]'))) {
-      return false;
-    }
-
+    if (_password == null) return false;
+    if (_password!.length < 8 || _password!.length > 16) return false;
+    if (!_password!.contains(new RegExp(r'[A-Z]'))) return false;
+    if (!_password!.contains(new RegExp(r'[a-z]'))) return false;
+    if (!_password!.contains(new RegExp(r'[0-9]'))) return false;
     return true;
   }
 
