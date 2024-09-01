55eaf9c95dde37b0fef1969ecb6019b5e8d58332
OmarElati <5796@holbertonstudents.com>
2024-09-01 14:14:19 +0100

    Greet USer

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,32 @@
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    String userData = await fetchUserData();
+
+    Map userMap = jsonDecode(userData);
+
+    return 'Hello ${userMap['username']}';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool hasValidCredential = await checkCredentials();
+
+    if (hasValidCredential) {
+      print("There is a user: true");
+
+      return (await greetUser());
+    } else {
+      print("There is a user: false");
+
+      return 'Wrong credentials';
+    }
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
